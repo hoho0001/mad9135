0051d1ddc656ac105dcae494a5acc0f95342e3f7
Steve Griffith <griffis@algonquincollege.com>
2018-11-06 15:13:48 -0500

    add wk 11 to nav menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,7 +46,8 @@ module.exports = {
           '/modules/week7/',
           '/modules/week8/',
           '/modules/week9/',
-          '/modules/week10/'
+          '/modules/week10/',
+          '/modules/week11/'
         ]
       },
       {
